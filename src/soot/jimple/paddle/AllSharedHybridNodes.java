99c2b1e6eebd11d71716e5e7fbc8e050dd66c183
a5richar <a5richar@sable.mcgill.ca>
2006-06-09 13:51:00 +0000

    Forgot to check in AllSharedHybridNodes.java; sorry!

---
+++ src/soot/jimple/paddle/AllSharedHybridNodes.java
@@ -0,0 +1,61 @@
+package soot.jimple.paddle;
+
+import java.util.LinkedList;
+
+import soot.PaddleG;
+import soot.PaddleSingletons;
+
+/**A singleton to hold the hash table for SharedHybridSet*/
+
+public class AllSharedHybridNodes {
+    public AllSharedHybridNodes( PaddleSingletons.Global g ) {}
+    public static AllSharedHybridNodes v() { return PaddleG.v().soot_jimple_paddle_AllSharedHybridNodes(); }
+
+	public class BitVectorLookupMap {
+		// Each element i is a list of BitVectors which have i 1s
+		// (i elements in the set).
+		// But should this be a LinkedList or some kind of Set?
+		// I'll try LinkedList
+		// TODO: Maybe implement my own linked list here
+		// -it would need an add method and an iterator
+
+		public LinkedList[] map = new LinkedList[1];
+
+		private final static int INCREASE_FACTOR = 2;  //change to affect the
+			//speed/memory tradeoff
+		
+		public void add(int size, PointsToBitVector toAdd) {
+			if (map.length < size + 1)
+			// if the `map' array isn't big enough
+			{
+				// TODO: The paper says it does some rearranging at this point
+				LinkedList[] newMap = new LinkedList[size * INCREASE_FACTOR];
+				System.arraycopy(map, 0, newMap, 0, map.length);
+				map = newMap;
+			}
+
+			if (map[size] == null)
+				map[size] = new LinkedList();
+			map[size].add(toAdd);
+		}
+		
+		public void remove(int size, PointsToBitVector toRemove)
+		{
+/*			if (map[size] == null)
+			{
+				//Can't happen
+				System.out.println(toRemove.cardinality());
+			}
+*/
+			map[size].remove(toRemove);
+		}
+	}
+
+
+	public BitVectorLookupMap lookupMap = new BitVectorLookupMap(); // A hash table of all
+	// the bit vectors for all points-to sets.
+	// It can keep growing as more bit vectors are added to it, which
+	// means it will have to occasionally double in size, which is expensive.
+
+
+}
